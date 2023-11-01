770b0e1c5f8efd586e08b5d1aedec681b5e58e3f
mmmutia <mutiabudiutami@gmail.com>
2023-11-01 22:08:47 +0700

    update footer.dart

--- lib/components/footer.dart
+++ lib/components/footer.dart
@@ -1,3 +1,68 @@
+// import 'package:flutter/material.dart';
+
+// class Footer extends StatefulWidget {
+//   const Footer({Key? key}) : super(key: key);
+
+//   @override
+//   _FooterState createState() => _FooterState();
+// }
+
+// class _FooterState extends State<Footer> {
+//   int _selectedIndex = 0; // Indeks awal (Beranda)
+
+//   void _onItemTapped(int index) {
+//     setState(() {
+//       _selectedIndex = index;
+//     });
+
+//     switch (_selectedIndex) {
+//       case 0:
+//         Navigator.of(context)
+//             .pushNamed('/main'); // Ganti dengan rute yang sesuai
+//         break;
+//       case 1:
+//         Navigator.of(context)
+//             .pushNamed('/faq'); // Ganti dengan rute yang sesuai
+//         break;
+//       case 2:
+//         Navigator.of(context)
+//             .pushNamed('/contact'); // Ganti dengan rute yang sesuai
+//         break;
+//     }
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return BottomNavigationBar(
+//       currentIndex: _selectedIndex,
+//       onTap: _onItemTapped,
+//       items: [
+//         BottomNavigationBarItem(
+//           icon: Icon(
+//             Icons.home,
+//             color: _selectedIndex == 0 ? Colors.blue : null, // Beranda
+//           ),
+//           label: 'Beranda',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(
+//             Icons.question_answer_outlined,
+//             color: _selectedIndex == 1 ? Colors.blue : null, // FAQ
+//           ),
+//           label: 'FAQ',
+//         ),
+//         BottomNavigationBarItem(
+//           icon: Icon(
+//             Icons.contacts,
+//             color: _selectedIndex == 2 ? Colors.blue : null, // Kontak
+//           ),
+//           label: 'Kontak',
+//         ),
+//       ],
+//     );
+//   }
+// }
+
 import 'package:flutter/material.dart';
 
 class Footer extends StatefulWidget {
@@ -9,6 +74,23 @@ class Footer extends StatefulWidget {
 
 class _FooterState extends State<Footer> {
   int _selectedIndex = 0; // Indeks awal (Beranda)
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+
+    // Ambil nama rute halaman yang sedang aktif
+    final currentRoute = ModalRoute.of(context)!.settings.name;
+
+    // Tentukan _selectedIndex berdasarkan nama rute halaman yang sedang aktif
+    if (currentRoute == '/faq') {
+      _selectedIndex = 1;
+    } else if (currentRoute == '/contact') {
+      _selectedIndex = 2;
+    } else {
+      _selectedIndex = 0; // Default ke Beranda jika tidak ada rute yang cocok
+    }
+  }
 
   void _onItemTapped(int index) {
     setState(() {
