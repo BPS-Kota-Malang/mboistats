1fe7bf10d40824e9b82f42b33e9168c4560a4636
Ananda Zakia S <117088186+zakiaass@users.noreply.github.com>
2023-11-01 21:21:52 +0700

    Up Sub Menu IPM

--- lib/datas/ipm.dart
+++ lib/datas/ipm.dart
@@ -1,9 +1,9 @@
 import 'package:mboistat/models/ipm.dart';
 
 List<IPM> ipm = [
-  IPM('ipm_1.png', 'Indeks Pembangunan Manusia', 'Penduduk bekerja sementara', route: '/PendudukBekerja'),
-  IPM('ipm_2.png', 'Usia Harapan Hidup', 'Presentase angkatan kerja terhadap usia kerja', route: '/UsiaHarapanHidup'),
-  IPM('ipm_3.png', 'Harapan Lama Sekolah', 'Presentase pengangguran angkatan kerja', route: '/HarapanLamaSekolah'),
-  IPM('ipm_4.png', 'Rata-rata Lama Sekolah', 'Penduduk yang tidak punya pekerjaan', route: '/RataRataLamaSekolah'),
-  IPM('ipm_5.png', 'Daya Beli', 'Penduduk yang tidak punya pekerjaan', route: '/DayaBeli'),
+  IPM('ipm_1.png', 'Indeks Pembangunan Manusia', 'Indikator kualitas hidup', route: '/PendudukBekerja'),
+  IPM('ipm_2.png', 'Usia Harapan Hidup', 'Rata-rata seseorang hidup', route: '/UsiaHarapanHidup'),
+  IPM('ipm_3.png', 'Harapan Lama Sekolah', 'Lamanya sekolah yang diharapkan', route: '/HarapanLamaSekolah'),
+  IPM('ipm_4.png', 'Rata-rata Lama Sekolah', 'Lamanya pendidikan formal', route: '/RataRataLamaSekolah'),
+  IPM('ipm_5.png', 'Daya Beli', 'Rata-rata pengeluaran riil', route: '/DayaBeli'),
 ];
