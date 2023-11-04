88213728a38ddcf90be932e670675142abb87e50
mmmutia <mutiabudiutami@gmail.com>
2023-11-04 10:53:45 +0700

    restore stash

--- lib/datas/button.dart
+++ lib/datas/button.dart
@@ -2,9 +2,16 @@ import 'package:mboistat/models/button.dart';
 
 //ini dibangun menggunakan contruktor
 List<MboistatButton> mboistatButton = [
+<<<<<<< Updated upstream
   MboistatButton(button: 'berita', title: 'Berita Resmi Statistik', route: '/berita'),
   MboistatButton(button: 'infografis', title: 'Infografis', route: '/infografis'),
-  MboistatButton(button: 'tentang', title: 'Tentang Kami', route: '/tentang'),
+=======
+  MboistatButton(
+      button: 'berita', title: 'Berita Resmi Statistik', route: '/berita'),
   MboistatButton(button: 'publikasi', title: 'Publikasi', route: '/publikasi'),
+  MboistatButton(
+      button: 'infografis', title: 'Infografis', route: '/infografis'),
+>>>>>>> Stashed changes
+  MboistatButton(button: 'tentang', title: 'Tentang Kami', route: '/tentang'),
   MboistatButton(button: 'lainnya', title: 'Lainnya', route: '/more'),
 ];
--- lib/models/publikasi.dart
+++ lib/models/publikasi.dart
@@ -1,5 +1,15 @@
+<<<<<<< Updated upstream
 class ImageData {
   final String imagePath;
 
   ImageData({required this.imagePath});
 }
+=======
+class Publikasi {
+  final String images;
+
+  Publikasi(this.images);
+
+  String get imagePath => null;
+}
+>>>>>>> Stashed changes
