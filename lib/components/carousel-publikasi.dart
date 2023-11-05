8343cbd18ebe951f03511137c4ccf9d0e69b771b
FahrezaDaffa93 <117092951+FahrezaDaffa93@users.noreply.github.com>
2023-11-05 22:03:05 +0700

    carousel berita dan publikasi

--- lib/components/carousel-publikasi.dart
+++ lib/components/carousel-publikasi.dart
@@ -1,52 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class CarouselPublikasi extends StatelessWidget {
-  const CarouselPublikasi({Key? key}) : super(key: key);
+class CarouselPublikasi extends StatefulWidget {
+  @override
+  _CarouselPublikasiState createState() => _CarouselPublikasiState();
+}
+
+class _CarouselPublikasiState extends State<CarouselPublikasi> {
+  List<Map<String, dynamic>> dataPublikasi = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(
+      Uri.parse(
+          'https://webapi.bps.go.id/v1/api/list/domain/3573/model/publication/lang/ind/id/1/key/9db89e91c3c142df678e65a78c4e547f'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final images = (data['data'][1] as List).cast<Map<String, dynamic>>();
+      setState(() {
+        dataPublikasi = images;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      padding: const EdgeInsets.all(16.0), // Padding untuk Carousel
-      color: Colors.grey[
-          200], // Latar belakang abu-abu (ganti dengan warna yang Anda inginkan)
-      child: Column(
-        children: [
-          Text(
-            'PUBLIKASI',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 16), // Jarak antara teks dan Carousel
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 200, // Sesuaikan tinggi sesuai kebutuhan Anda
-              enlargeCenterPage: true,
-              autoPlay: true, // Aktifkan otomatis bermain
-              aspectRatio:
-                  16 / 9, // Sesuaikan rasio aspek sesuai kebutuhan Anda
-            ),
-            items: [
-              // Daftar widget yang akan ditampilkan dalam carousel
-              Image.asset(
-                'assets/images/Rectangle8.png',
-                fit: BoxFit
-                    .cover, // Sesuaikan tampilan gambar sesuai kebutuhan Anda
+    return dataPublikasi.isEmpty
+        ? Center(child: CircularProgressIndicator()) // Indicator saat sedang memuat
+        : Column(
+            children: [
+              Text(
+                'PUBLIKASI', // Judul "Infografis"
+                style: TextStyle(
+                  fontSize: 24, // Sesuaikan ukuran teks dengan preferensi Anda
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Image.asset(
-                'assets/images/gambar2.png',
-                fit: BoxFit.cover,
-              ),
-              Image.asset(
-                'assets/images/gambar3.png',
-                fit: BoxFit.cover,
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 450, // Sesuaikan dengan tinggi kertas A4 dalam piksel
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 3 / 4, // Rasio aspek 3:4 untuk layout kertas A4
+                ),
+                items: dataPublikasi.map((item) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width, // Lebar gambar mengikuti lebar layar
+                    child: Image.network(
+                      item['cover'],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }).toList(),
               ),
             ],
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
+
+
