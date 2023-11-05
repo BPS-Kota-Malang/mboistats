53bdfce543a8254c8f52807c82e2d1a3dcab40d0
FahrezaDaffa93 <117092951+FahrezaDaffa93@users.noreply.github.com>
2023-11-05 21:51:54 +0700

    carousel-infografis

--- lib/components/carousel-infografis.dart
+++ lib/components/carousel-infografis.dart
@@ -1,50 +1,72 @@
 import 'package:flutter/material.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 import 'package:carousel_slider/carousel_slider.dart';
 
-class CarouselInfografis extends StatelessWidget {
-  const CarouselInfografis({Key? key}) : super(key: key);
+class CarouselInfografis extends StatefulWidget {
+  @override
+  _CarouselInfografisState createState() => _CarouselInfografisState();
+}
+
+class _CarouselInfografisState extends State<CarouselInfografis> {
+  List<Map<String, dynamic>> dataInfografis = [];
+
+  @override
+  void initState() {
+    super.initState();
+    fetchData();
+  }
+
+  Future<void> fetchData() async {
+    final response = await http.get(
+      Uri.parse(
+          'https://webapi.bps.go.id/v1/api/list/domain/3573/model/infographic/lang/ind/domain/3573/key/9db89e91c3c142df678e65a78c4e547f'),
+    );
+
+    if (response.statusCode == 200) {
+      final data = json.decode(response.body);
+      final images = (data['data'][1] as List).cast<Map<String, dynamic>>();
+      setState(() {
+        dataInfografis = images;
+      });
+    } else {
+      throw Exception('Failed to load data');
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          Text(
-            'INFOGRAFIS STATISTIK',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.bold,
-            ),
-          ),
-          SizedBox(height: 16), // Jarak antara teks dan Carousel
-          CarouselSlider(
-            options: CarouselOptions(
-              height: 200, // Sesuaikan tinggi sesuai kebutuhan Anda
-              enlargeCenterPage: true,
-              autoPlay: true, // Aktifkan otomatis bermain
-              aspectRatio:
-                  16 / 9, // Sesuaikan rasio aspek sesuai kebutuhan Anda
-            ),
-            items: [
-              // Daftar widget yang akan ditampilkan dalam carousel
-              Image.asset(
-                'assets/images/Rectangle8.png',
-                fit: BoxFit
-                    .cover, // Sesuaikan tampilan gambar sesuai kebutuhan Anda
+    return dataInfografis.isEmpty
+        ? Center(child: CircularProgressIndicator()) // Indicator saat sedang memuat
+        : Column(
+            children: [
+              Text(
+                'INFOGRAFIS', // Judul "Infografis"
+                style: TextStyle(
+                  fontSize: 24, // Sesuaikan ukuran teks dengan preferensi Anda
+                  fontWeight: FontWeight.bold,
+                ),
               ),
-              Image.asset(
-                'assets/images/gambar2.png',
-                fit: BoxFit.cover,
-              ),
-              Image.asset(
-                'assets/images/gambar3.png',
-                fit: BoxFit.cover,
+              CarouselSlider(
+                options: CarouselOptions(
+                  height: 450, // Sesuaikan dengan tinggi kertas A4 dalam piksel
+                  enlargeCenterPage: true,
+                  autoPlay: true,
+                  aspectRatio: 3 / 4, // Rasio aspek 3:4 untuk layout kertas A4
+                ),
+                items: dataInfografis.map((item) {
+                  return Container(
+                    width: MediaQuery.of(context).size.width, // Lebar gambar mengikuti lebar layar
+                    child: Image.network(
+                      item['img'],
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                }).toList(),
               ),
             ],
-          ),
-        ],
-      ),
-    );
+          );
   }
 }
+
+
--- lib/pages/infografis-pages.dart
+++ lib/pages/infografis-pages.dart
@@ -1,66 +1,118 @@
 import 'package:flutter/material.dart';
-import 'package:mboistat/datas/infografis.dart';
-import 'package:mboistat/theme.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 
-class InfografisPages extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: Text('Infografis'),
-          leading: IconButton(
-            icon: Image.asset(
-              'assets/icons/left-arrow.png',
-              height: 18,
-            ),
-            onPressed: () {
-              Navigator.of(context)
-                  .pop(); // Menavigasi kembali ke halaman sebelumnya
-            },
-          ),
-        ),
-        body: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-             const SizedBox(
-              height: 24,
-            ),
-            Center(
-              child: Padding(
-                padding:
-                  const EdgeInsets.only(bottom: 16.0), // Jarak di bawah judul
-                child: Text(
-                  'INFOGRAFIS TERBARU', // Ganti dengan judul yang sesuai
-                  style: bold16.copyWith(color: dark1),
-              ),
-            ),
-            ),
+class Infografis {
+  final String gambarInfografis;
 
-            Expanded(
-              child: Center(
-                child: GridView.builder(
-                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2, // Menampilkan 2 kolom
-                    mainAxisSpacing: 0.0, // Jarak vertikal antara elemen-elemen dalam grid
-                    crossAxisSpacing: 0.0, // Jarak horizontal antara elemen-elemen dalam grid
-                  ),
-                  itemCount: images.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(bottom: 24, left: 16, right: 16), 
-                      child: Image.asset(
-                        images[index].imagePath,
-                        // width: 600,
-                        // height: 600,
-                         fit: BoxFit.cover, // Sesuaikan dengan tata letak gambar
-                      ),
-                    );
-                  },
-                ),
-              ),
-            ),
-          ],
-        ),
+  Infografis({required this.gambarInfografis});
+
+  factory Infografis.fromJson(Map<String, dynamic> json) {
+    return Infografis(
+      gambarInfografis: json['gambarInfografis'],
     );
   }
 }
+
+
+
+class InfografisPages extends StatefulWidget {
+  @override
+  _InfografisPagesState createState() => _InfografisPagesState();
+}
+
+class _InfografisPagesState extends State<InfografisPages> {
+  // Deklarasikan variabel untuk menyimpan data infografis dari API
+  List<Infografis> infografisData = [];
+
+  @override
+  void initState() {
+    // Panggil fungsi untuk mengambil data infografis dari API di sini
+    fetchDataFromAPI();
+    super.initState();
+  }
+
+  void fetchDataFromAPI() async {
+    try {
+      final response = await http.get(
+        Uri.parse(
+            'https://webapi.bps.go.id/v1/api/list/domain/3573/model/infographic/lang/ind/domain/3573/key/9db89e91c3c142df678e65a78c4e547f'),
+      );
+
+      if (response.statusCode == 200) {
+        final data = json.decode(response.body);
+
+        setState(() {
+          infografisData = List<Infografis>.from(
+              data['data'].map((data) => Infografis.fromJson(data)));
+        });
+      } else {
+        print('HTTP error ${response.statusCode}');
+      }
+    } catch (e) {
+      print('Error: $e');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Infografis'),
+        leading: IconButton(
+          icon: Image.asset(
+            'assets/icons/left-arrow.png',
+            height: 18,
+          ),
+          onPressed: () {
+            Navigator.of(context).pop();
+          },
+        ),
+      ),
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.stretch,
+        children: [
+          const SizedBox(
+            height: 24,
+          ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.only(
+                  bottom: 16.0),
+              child: Text(
+                'INFOGRAFIS TERBARU',
+                style: TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: Center(
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  mainAxisSpacing: 0.0,
+                  crossAxisSpacing: 0.0,
+                ),
+                itemCount: infografisData.length,
+                itemBuilder: (context, index) {
+                  return Padding(
+                    padding: const EdgeInsets.only(bottom: 24, left: 16, right: 16),
+                    child: Image.network(
+                      infografisData[index].gambarInfografis,
+                      fit: BoxFit.cover,
+                    ),
+                  );
+                },
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+
