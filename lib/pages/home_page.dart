19463546dfa3bca67595dabc028128306a510d01
Ananda Zakia S <117088186+zakiaass@users.noreply.github.com>
2023-11-08 20:25:35 +0700

    Add AlertDialogExit

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:mboistat/components/buttonSection.dart';
 import 'package:mboistat/components/carousel-infografis.dart';
 import 'package:mboistat/components/carousel-publikasi.dart';
@@ -9,54 +10,91 @@ import 'package:mboistat/theme.dart';
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
+  Future<bool> _onWillPop(BuildContext context) async {
+    return (await showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            title: Text(
+            'Konfirmasi Keluar',
+            style: TextStyle(color: Colors.blue), // Warna teks biru
+          ),
+          content: Text('Apakah Anda yakin ingin keluar dari aplikasi?'),
+            actions: <Widget>[
+            OutlinedButton(
+              onPressed: () => Navigator.of(context).pop(false),
+              style: OutlinedButton.styleFrom(
+                side: BorderSide(color: Colors.blue), // Warna border biru
+              ),
+              child: Text(
+                'Tidak',
+                style: TextStyle(color: Colors.blue), // Warna teks biru
+              ),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                // Close the entire app when 'Ya' is selected
+                SystemNavigator.pop(); // Use this to exit the app
+              },
+              child: Text('Ya'),
+            ),
+          ],
+          ),
+        )) ??
+        false;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Color.fromARGB(255, 255, 255, 255),
-      appBar: AppBar(
-        backgroundColor: Colors.white,
-        elevation: 0,
-        toolbarHeight: 50,
-        title: const Text(
-          'MBOIStatS+',
-          style: TextStyle(color: Colors.black),
-        ),
-        leading: Row(
-          mainAxisAlignment: MainAxisAlignment.end,
-          children: [
-            Image.asset('assets/images/Mbois-stat Logo_Fix Putih.png',
-                width: 40, height: 40),
-          ],
-        ),
-      ),
-      body: SingleChildScrollView(
-          child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
-        children: [
-          Padding(
-            padding: EdgeInsets.all(16.0), // Tambahkan jarak 16 di semua sisi
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Yuk lebih dekat dengan BPS Kota Malang',
-                  style: bold16.copyWith(color: dark1),
-                ),
-                SizedBox(height: 8.0), // Tambahkan jarak antara kalimat
-                Text(
-                  'Mau cari data apa ???',
-                  style: regular14.copyWith(color: dark2),
-                ),
-              ],
-            ),
+    return WillPopScope(
+      onWillPop: () => _onWillPop(context),
+      child: Scaffold(
+        backgroundColor: Color.fromARGB(255, 255, 255, 255),
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          elevation: 0,
+          toolbarHeight: 50,
+          title: const Text(
+            'MBOIStatS+',
+            style: TextStyle(color: Colors.black),
           ),
-          Menus(),
-          ButtonSection(),
-          CarouselPublikasi(),
-          CarouselInfografis(),
-        ],
-      )),
-      bottomNavigationBar: Footer(),
+          leading: Row(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              Image.asset('assets/images/Mbois-stat Logo_Fix Putih.png',
+                  width: 40, height: 40),
+            ],
+          ),
+        ),
+        body: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Padding(
+                padding: EdgeInsets.all(16.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text(
+                      'Yuk lebih dekat dengan BPS Kota Malang',
+                      style: bold16.copyWith(color: dark1),
+                    ),
+                    SizedBox(height: 8.0),
+                    Text(
+                      'Mau cari data apa ???',
+                      style: regular14.copyWith(color: dark2),
+                    ),
+                  ],
+                ),
+              ),
+              Menus(),
+              ButtonSection(),
+              CarouselPublikasi(),
+              CarouselInfografis(),
+            ],
+          ),
+        ),
+        bottomNavigationBar: Footer(),
+      ),
     );
   }
 }
