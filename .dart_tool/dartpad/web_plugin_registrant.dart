44d72150aa30c9159305a20c3e9d89feb0ad09ac
scaitz <scaitz.it07@gmail.com>
2025-01-23 10:09:10 +0700

    perubahan data

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -10,6 +10,7 @@ import 'package:connectivity_for_web/connectivity_for_web.dart';
 import 'package:device_info_plus/src/device_info_plus_web.dart';
 import 'package:file_picker/_internal/file_picker_web.dart';
 import 'package:fluttertoast/fluttertoast_web.dart';
+import 'package:open_file_web/open_file_web.dart';
 import 'package:syncfusion_pdfviewer_web/pdfviewer_web.dart';
 import 'package:toast/toast_web.dart';
 import 'package:url_launcher_web/url_launcher_web.dart';
@@ -21,6 +22,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   DeviceInfoPlusWebPlugin.registerWith(registrar);
   FilePickerWeb.registerWith(registrar);
   FluttertoastWebPlugin.registerWith(registrar);
+  OpenFilePlugin.registerWith(registrar);
   SyncfusionFlutterPdfViewerPlugin.registerWith(registrar);
   ToastWebPlugin.registerWith(registrar);
   UrlLauncherPlugin.registerWith(registrar);
